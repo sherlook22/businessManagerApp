bad64b1f3e974a430c37921a8617dfae0da6ff10
Gonzalo Re <gonzalore01@gmail.com>
2021-06-04 00:09:50 -0300

    feat: ensure SignUpController returs 400 is no email is provided

---
+++ src/presentation/controllers/signup/signup-controller.ts
@@ -0,0 +1,13 @@
+import { Controller } from '../../protocols/controller'
+import { HttpRequest, HttpResponse } from '../../protocols/http'
+
+export class SignUpController implements Controller {
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error('email')
+      }
+    }
+  }
+}
