0875c4328a4da6cc8cc9af7d0debc2c355d773dc
Gonzalo Re <gonzalore01@gmail.com>
2021-06-06 11:42:36 -0300

    test: ensure SignUpController returs 400 is no name is provided

--- src/presentation/controllers/signup/signup-contoller.spec.ts
+++ src/presentation/controllers/signup/signup-contoller.spec.ts
@@ -15,4 +15,19 @@ describe('SignUpController', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('email'))
   })
+
+  test('Should return 400 is no name is provided', async () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        email: 'any_name',
+        lastName: 'lastName',
+        password: 'password',
+        passwordConfirmation: 'passwordConfirmation'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('name'))
+  })
 })
